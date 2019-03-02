9f23f17973171d8aa0de225536ec96e9ae49ccb4
alexander_maksimenko <mclander@list.ru>
2019-03-02 13:05:43 +0300

    handmade fixes

--- tab-react-redux-challenge/precache-manifest.c1a6160284610a56ec6be14a9f71686e.js
+++ tab-react-redux-challenge/precache-manifest.c1a6160284610a56ec6be14a9f71686e.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
     "revision": "3a3d5af5dec1c4ea4740",
-    "url": "/static/js/runtime~main.3a3d5af5.js"
+    "url": "./static/js/runtime~main.3a3d5af5.js"
   },
   {
     "revision": "f50ad1c4427114e81ead",
-    "url": "/static/js/main.f50ad1c4.chunk.js"
+    "url": "./static/js/main.f50ad1c4.chunk.js"
   },
   {
     "revision": "43acabc0bb3393f630b7",
-    "url": "/static/js/3.43acabc0.chunk.js"
+    "url": "./static/js/3.43acabc0.chunk.js"
   },
   {
     "revision": "871d44bdd75968551f4e",
-    "url": "/static/js/2.871d44bd.chunk.js"
+    "url": "./static/js/2.871d44bd.chunk.js"
   },
   {
     "revision": "f50ad1c4427114e81ead",
-    "url": "/static/css/main.257387c9.chunk.css"
+    "url": "./static/css/main.257387c9.chunk.css"
   },
   {
     "revision": "871d44bdd75968551f4e",
-    "url": "/static/css/2.fff9da86.chunk.css"
+    "url": "./static/css/2.fff9da86.chunk.css"
   },
   {
     "revision": "5f451fa1394da4de8d241f753ac61b84",
-    "url": "/index.html"
+    "url": "./index.html"
   }
 ];
